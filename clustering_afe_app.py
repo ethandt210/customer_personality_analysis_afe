b3d9979f182d67c88001818894301370aa08957e
ethandtimadius <t-ethan.timadius@traveloka.com>
2025-02-21 15:39:36 +0700

    turn off PCA

--- clustering_afe_app.py
+++ clustering_afe_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 # import plotly.express as px
 import re
-from sklearn.decomposition import PCA
+# from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import time
 
